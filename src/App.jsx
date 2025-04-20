d014e1948d0fc0942e4e3e2022f8f22ede251da7
Meet Dixit <infomeet06@gmail.com>
2025-04-20 19:09:03 +0530

    Updated API URL to Render server

--- src/App.jsx
+++ src/App.jsx
@@ -118,7 +118,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/chat", {
+      const response = await fetch("https://agri-sakha-server.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
